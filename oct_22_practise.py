610b21af7d004dabed9abc6d00682aa4973c1e26
vamshi-krishna122 <vk7937190@gmail.com>
2025-10-23 12:25:38 +0530

    first commit

---
+++ oct_22_practise.py
@@ -0,0 +1,40 @@
+
+# Unpack dictionary using while loop
+
+emp_details = {"name":"Jack" , "age": 19 , "salary":"35000"}
+
+emp_keys = list(emp_details.keys())
+
+start = 0
+
+while start < len(emp_keys):
+    key = emp_keys[start]
+    print(key , ":" , emp_details[key])
+    start += 1
+
+
+
+lst = ['harsha', 'ranjth', 'kiran'] 
+
+for item in lst:
+    for each in item:
+        print(each)
+
+
+
+lst = [3,0,6,33,55,11,2,4,77,9,10,100]
+
+for idx1 in range(len(lst)):
+    for idx2 in range(idx1+1 , len(lst)):
+        if lst[idx1]>lst[idx2]:
+            lst[idx1], lst[idx2] = lst[idx2],lst[idx1]
+
+print("Sorted List",lst)
+
+                      
+
+
+
+"""# how to unpack the dictionary using while loop
+
+sort the list usign bubble sort"""
---
+++ oct_23_practise.py
@@ -0,0 +1,11 @@
+def add(a,b):
+    return a+b
+
+def add_multiply(a,b):
+    return add(a,b) * 2
+
+def add_div(a,b):
+    return  add(a,b) / 2
+
+print(add_multiply(2,3))
+print(add_div(5,6))
